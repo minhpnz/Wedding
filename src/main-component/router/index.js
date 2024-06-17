cae8aaaeacbfffbde2e66706c6693f70d77aca6d
minhpnz <minh.phan81299@gmail.com>
2024-06-17 16:04:04 +0700

    add

--- src/main-component/router/index.js
+++ src/main-component/router/index.js
@@ -45,7 +45,7 @@ const AllRoute = () => {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Homepage/>} />
+          <Route path='/Wedding' element={<Homepage/>} />
           <Route path='home' element={<Homepage/>} />
           <Route path='home2' element={<Homepage2/>} />
           <Route path='home3' element={<HomePage3/>} />
